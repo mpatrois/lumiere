5b94ebd0becc6aba1b6488b060eceb12b9b4089b
Hubert Moncenis <hubmoncenis@gmail.com>
2018-11-15 16:34:33 +0100

    :package: node sass + scss loader / :sparkles: adding Uploader componnent

---
+++ front/src/views/Uploader.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h1>UPLOADER VIEW</h1>
+    <form>
+      <fieldset>
+        <input type="text" placeholder="Titre de la vidéo">
+        <textarea placeholder="Description de la vidéo" />
+      </fieldset>
+      <label for="title">
+      </label>
+      <label for="video"></label>
+      Votre vidéo : 
+      <input type="file" id="video" />
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Uploader',
+};
+</script>
+
+
+<style lang="scss">
+  fieldset {
+    border: none;
+  }
+</style>
