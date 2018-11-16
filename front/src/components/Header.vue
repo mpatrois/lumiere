389a5cfa6f4701c4ab7a54eeb34eee009835137f
Hubert Moncenis <hubmoncenis@gmail.com>
2018-11-16 11:21:00 +0100

    :lipstick: header design

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -5,21 +5,18 @@
     </router-link>
     <input type="text" v-model="searchText" class="searchBar" placeholder="Search here..." @keypress.enter="goToSearch"/>
     <div class="header__actions">
-      <router-link to="/upload">
-        <img src="https://image.flaticon.com/icons/svg/149/149185.svg" alt="" class="tmpIconHeader">
-      </router-link>
-      <div class="notifs">
-        <img src="https://image.flaticon.com/icons/svg/633/633584.svg" alt="" class="tmpIconHeader" @click="displayNotifs">
-        <div class="notifs__content">
-          Pas de notifs disponibles
-        </div>
-      </div>
-      <div class="profile">
+      <!-- <router-link to="/upload"> -->
+        <img src="../assets/add.svg">
+      <!-- </router-link> -->
+      <img src="../assets/feed.svg" alt="">
+      <img src="../assets/notif.svg" alt="">
+      <img src="../assets/profile.svg" alt="">
+      <!-- <div class="profile">
         <img src="https://image.flaticon.com/icons/svg/78/78373.svg" alt="" class="tmpIconHeader" @click="displayProfile">
         <div class="profile__content">
           Pas d'informations profile
         </div>
-      </div>
+      </div> -->
     </div>
   </header>
 </template>
