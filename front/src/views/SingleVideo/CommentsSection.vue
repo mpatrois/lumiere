588b2c9af0357e7b8f69a7638d03b490045ab5c7
rebenga2 <rebenga2@ECV-BF-DIG-025.local>
2018-11-16 11:01:14 +0100

    some css

--- front/src/views/SingleVideo/CommentsSection.vue
+++ front/src/views/SingleVideo/CommentsSection.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="comments">
-        <input type="text" placeholder="ajouter un commentaire">
+        <input type="text" placeholder="Ajouter un commentaire">
         <div>
-            <h3>TOP COMMENTAIRE</h3>
+            <h3>TOP COMMENTAIRES</h3>
             <ul>
-                <li v-for="(comment, index) in comments" :key="index">
+                <li class="comment" v-for="(comment, index) in comments" :key="index">
                     <div class="user-section">
                         <img :src="comment.user.url_avatar">
                         <div class="user-infos">
                             <h4>
                                 {{comment.user.name}}
-                                <span>{{comment.date}}</span>
+                                <span class="info-date">{{comment.date}}</span>
                             </h4>
                             <div>
-                                <span>{{comment.nbComments}}</span>
-                                <span>{{comment.nbLikes}}</span>
+                                <span>{{comment.nbLikes}} likes</span>
+                                <span>{{comment.nbComments}} commentaires</span>
                             </div>
                         </div>
                     </div>
-                    <div>
+                    <div class="comment-content">
                         {{comment.content}}
                     </div>
                 </li>
@@ -37,12 +37,31 @@ export default {
 .comments{
     background: #2E2F30;
     color:white;
+    padding: 20px;
     h3{
         font-weight: 700;
+        padding-bottom: 10px;
+    }
+    input{
+        width: 100%;
+        display: inline-block;
+        background: transparent;
+        border: 0px;
+        border-bottom: 1px solid white;
+        padding-bottom: 10px;
+        color:white;
+        outline: 0px;
+        margin-bottom: 40px;
+    }
+    .comment{
+        margin-bottom: 10px;
     }
     .user-section{
+        text-align: left;
         display: flex;
         flex-direction: row;
+        font-size: 12px;
+        margin-bottom: 10px;
         img{
             // flex: 0;
             width: 30px;
@@ -54,7 +73,13 @@ export default {
                 margin: 0px;
                 padding: 0px 10px;
             }
+            .info-date{
+                font-size: 10px;
+            }
         }
+    }
+    .comment-content{
+        font-size: 12px;
     }
 }
 </style>
